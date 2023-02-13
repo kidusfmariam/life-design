f0a7ae41768c0620219117443471811b8369b4a5
kidusfmariam <99491498+kidusfmariam@users.noreply.github.com>
2023-02-12 18:55:39 -0800

    footer section complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import About from "./Pages/About"
 import Works from "./Pages/Works"
 import Contact from "./Pages/Contact"
 import Navbar from "./Components/Navbar/Navbar"
+import Footer from "./Components/Footer/Footer"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/about" element={<About/>} />
         <Route path="/contact" element={<Contact/>} />
       </Routes>
+        <Footer/>
     </Router>
   )
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className='footer-wrapper'>
+            <div className='footer-main'>
+                <h1>LIFE.</h1>
+                <div className='footer-nav'>
+                    <p>Home</p>
+                    <p>Works</p>
+                    <p>About</p>
+                    <p>Contact</p>
+                </div>
+                   
+                
+            </div>
+            <p>Designed and developed by <a href="https://github.com/kidusfmariam" target='_blank'>Kidus Ayalew.</a></p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
