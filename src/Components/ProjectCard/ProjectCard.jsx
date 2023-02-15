1456a89932df6d04b28cda7c4dc8279f116df39a
kidusfmariam <99491498+kidusfmariam@users.noreply.github.com>
2023-02-14 16:44:14 -0800

    'Works' page complete with animations

--- src/Components/ProjectCard/ProjectCard.jsx
+++ src/Components/ProjectCard/ProjectCard.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import './ProjectCard.css'
+import { motion } from 'framer-motion'
 const ProjectCard = ({id, image, title}) => {
   return (
     <div className='project-card'>
+        <motion.div 
+            initial={{width: "100%"}}
+            animate={{width: 0}}
+            transition={{duration: 1.2, delay: 1.5 + (id*0.2)}}
+        className="white-overlay"></motion.div>
       <img className='project-img' src={image} alt={title} />
       <div className='project-card-overlay'>
             <h2>{title}</h2>
--- src/Containers/ProjectsContainer/Projects.jsx
+++ src/Containers/ProjectsContainer/Projects.jsx
@@ -1,18 +1,28 @@
 import './Projects.css'
 import projects from '../../../projects'
 import ProjectCard from '../../Components/ProjectCard/ProjectCard'
+import { easeInOut, motion } from 'framer-motion'
 const Projects = () => {
   return (
     <div className='projects-container'>
-        <div className='projects-overlay'></div>
+        <motion.div 
+          initial={{height: 0}}
+          animate={{height: "50vh"}}
+          transition={{duration: 1.2, ease: easeInOut}}
+        className='projects-overlay'></motion.div>
         <div className='projects-title'>
-            <h1>Our <span className='yellow'>projects.</span></h1>
+            <motion.h1
+              initial={{opacity: 0}}
+              animate={{opacity: 1}}
+              transition={{duration: 0.4, delay: 2}}
+            >Our <span className='yellow'>projects.</span></motion.h1>
         </div>
 
 
         <div className='projects-list'>
            {projects.map(item => (
               <ProjectCard
+                id={item.id}
                 key={item.id}
                 image={item.image}
                 title={item.title}
